c8fd5c73c3d5acfb7bf9d92f39482f4a836847b1
Sapir Nassimi <sapir.nassimi@gmail.com>
2019-06-01 18:15:55 +0300

    feat: add server class

---
+++ server.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+import deskew
+
+PORT = 8000
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+httpd = HTTPServer(('localhost', PORT), SimpleHTTPRequestHandler)
+print("listening on port " + str(PORT))
+httpd.serve_forever()
